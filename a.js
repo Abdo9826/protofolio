444933d9359ab335fa0705c298b3f2c03ab9ab17
Abdo9826 <102296263+Abdo9826@users.noreply.github.com>
2022-05-10 22:47:17 +0300

    fix bug

--- a.js
+++ a.js
@@ -1,12 +1,9 @@
 const humberger = document.querySelector('.humberger');
-
 const ulnav = document.querySelector('.ulnav');
+const logo = document.querySelector('.aa');
 
-const Protofolio = document.querySelector('.limenu1');
-const logo = document.querySelector('.aa')
-
-humberger.addEventListener("click", function () { 
-  logo.classList.toggle("none")
-  ulnav.classList.toggle("active") 
-  humberger.classList.toggle("active")
-})
+humberger.addEventListener('click', function () { 
+  logo.classList.toggle('none');
+  ulnav.classList.toggle('active'); 
+  humberger.classList.toggle('active');
+});
