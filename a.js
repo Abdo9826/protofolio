17188a715a64221cd316b177e303148d7b6868a7
Abdo9826 <102296263+Abdo9826@users.noreply.github.com>
2022-05-10 16:41:25 +0300

    Add menu to Porofile project

---
+++ a.js
@@ -0,0 +1,13 @@
+let humberger = document.querySelector(".humberger");
+
+let ulnav = document.querySelector(".ulnav");
+
+let logo = document.querySelector(".aa");
+
+let Protofolio = document.querySelector(".limenu1");
+
+humberger.addEventListener("click", function () { 
+    logo.classList.toggle("none")
+    ulnav.classList.toggle("active")
+    
+})
