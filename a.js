0ac14a6f0003e9ea69cf8b67b8ad801fbf10c166
Edubew <edubew98@gmail.com>
2022-05-10 20:22:57 +0300

    Add javascript code

--- a.js
+++ a.js
@@ -1,6 +1,6 @@
-let humberger = document.querySelector(".humberger");
+const humberger = document.querySelector(".humberger");
 
-let ulnav = document.querySelector(".ulnav");
+const ulnav = document.querySelector(".ulnav");
 
 let logo = document.querySelector(".aa");
 
@@ -8,6 +8,5 @@ let Protofolio = document.querySelector(".limenu1");
 
 humberger.addEventListener("click", function () { 
     logo.classList.toggle("none")
-    ulnav.classList.toggle("active")
-    
+    ulnav.classList.toggle("active") 
 })
