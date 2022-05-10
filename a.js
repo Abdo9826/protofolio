8222e29d72bfb7b001361d9153a7cf91a2cb0a39
Abdo9826 <102296263+Abdo9826@users.noreply.github.com>
2022-05-11 00:45:45 +0300

    Add js code

--- a.js
+++ a.js
@@ -1,9 +1,16 @@
 const humberger = document.querySelector('.humberger');
 const ulnav = document.querySelector('.ulnav');
 const logo = document.querySelector('.aa');
+const limenu = document.querySelector('.limenu');
 
 humberger.addEventListener('click', () => {
   logo.classList.toggle('none');
   ulnav.classList.toggle('active');
   humberger.classList.toggle('active');
+});
+
+limenu.addEventListener('click', () => {
+  ulnav.classList.remove('active');
+  humberger.classList.remove('active');
+  logo.classList.remove('none')
 });
