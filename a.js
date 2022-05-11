062c4bdc1d7ef9e37c03f70645696de4931b23d2
Abdo9826 <102296263+Abdo9826@users.noreply.github.com>
2022-05-11 21:59:26 +0300

    Add window poupul

--- a.js
+++ a.js
@@ -4,6 +4,11 @@ const logo = document.querySelector('.aa');
 const limenu = document.querySelector('.limenu');
 const limenu2 = document.querySelector('.limenu2');
 const limenu3 = document.querySelector('.limenu3');
+const mobile_button1 = document.querySelector('.btn1');
+const xbutton = document.querySelector('.xbutton');
+const mobile_window = document.querySelector ('.mobile-window');
+const mobile_button2 = document.querySelector('.btn2');
+
 
 humberger.addEventListener('click', () => {
   logo.classList.toggle('none');
@@ -27,4 +32,19 @@ limenu3.addEventListener('click', () => {
   ulnav.classList.remove('active');
   humberger.classList.remove('active');
   logo.classList.remove('none');
+});
+
+mobile_button2.addEventListener('click', ()=> {
+  mobile_window.style.display = 'block';
+  window.addEventListener('scroll', () => {
+    window.scrollTo(0,0);
+  })
+});
+
+mobile_button1.addEventListener('click', () => {
+  mobile_window.style.display = 'block';
+});
+
+xbutton.addEventListener('click', () => {
+  mobile_window.style.display = 'none';
 });
